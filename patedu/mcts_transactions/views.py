505669ebb1e9678bae4a0e8d41a8893f0d73eb2b
inferno <komalvis007g@gmail.com>
2015-07-25 11:50:42 +0530

    Minor

--- patedu/mcts_transactions/views.py
+++ patedu/mcts_transactions/views.py
@@ -546,7 +546,7 @@ def ODSANMANC(request):
 		#All ANC overdue services for given ANM
 		from django.db.models import Count
 		anc_benefs = ANCBenef.objects.filter(careprovider=anm)
-		anc_ods = OverDueEvents.objects.filter(beneficiary__in=anc_benefs).values('event').annotate(count=Count('event'))
+		anc_ods = OverDueEvents.objects.filter(beneficiary__in=anc_benefs, date=date_then).values('event').annotate(count=Count('event'))
 
 		anm_stats_anc = ''
 		for anc_od in anc_ods:
@@ -555,6 +555,6 @@ def ODSANMANC(request):
 			event = Events.objects.get(id=anc_od.get('event'))
 			event_count = anc_od.get('count')
 			anm_stats_anc += event.val+' '+str(event_count)
-		return HttpResponse(anm_stats_anc)
+		return HttpResponse(anm_stats_anc, content_type='text/plain')
 	else:
 		return HttpResponseBadRequest('HTTP method type not allowed')
